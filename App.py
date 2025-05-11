96ef15222356292183c98dab37d3a324b47bd35f
Anna <annaluzserena@protonmail.com>
2025-05-11 00:31:49 -0300

    Incisos A y C

--- App.py
+++ App.py
@@ -1,15 +1,15 @@
 #EJERCICIO 6 FARMACIA
 
 from datetime import datetime
-from TADventa import *
+from TadVenta import *
 from TADListaVentas import *
 from TADcolaObraSoc import *
 
-# ----- FUNCIONES PARA EL PROGRAMA ------
+# ------ FUNCIONES PARA EL PROGRAMA ------
 def MENU ():
 
     print("*** ADMINISTRAR LAS VENTAS FARMACEUTICAS ***\n\n" \
-    "1- Registrar Nueva Venta\n" \
+    "1- Registrar nuevas ventas\n" \
     "2- Modificar venta especifica\n" \
     "3- Eliminar venta especifica\n" \
     "4- Mostrar listado de ventas registradas\n" \
@@ -22,12 +22,24 @@ def MENU ():
     print("\n***********\n")
     return a  
 
-def imprimirLista_de_Ventas ():
-#codigo
+
+def imprimirVenta(venta):
+    #Imprimir los datos de una venta
+    fecha = verFechaHora(venta)
+    print(f"""\nCódigo del medicamento: {verCodigo(venta)}
+    Nombre del medicamento: {verNombre(venta)}
+    Droga: {verDroga(venta)}
+    Obra Social: {verObraSocial(venta)}
+    Plan: {verPlan(venta)}
+    Importe: {verImporte(venta)}
+    Fecha: {fecha.strftime("%d")}/{fecha.strftime("%m")}/{fecha.strftime("%Y")} {fecha.strftime("%X")}
+          \n""")
+
 def imprimirCola_de_ObrasSociales():
 #codigo
 
 # -- DECLARACION DE VARIABLES --
+lista_ventas = crearListaVentas()
 
 # ----- PROGRAMA PRINCIPAL -----
 print("\n\n*** BIENVENIDOS AL PROGRAMA REGISTRAR VENTAS FARMACEUTICAS *** \n\n")
@@ -37,9 +49,25 @@ opcion = MENU()
 #CAMBIAR POR CASE
 while (opcion != 0):
 
-    #Registrar Nueva venta y cargarla en la ListaVentas
-    if ( opcion == 1):
-        #aplicar descuento del 20% a plan especifico
+    if (opcion == 1):
+    #Registrar nuevas ventas y cargarlas en la lista de ventas
+        print("Ingreso de nuevas ventas de medicamentos. \n")
+        s = "si"
+        while (s == "si"):
+            print("Carga de una venta nueva. \n")
+            venta = crearVenta()
+            cod = int(input("Ingrese el código del medicamento: "))
+            nom = input("Ingrese el nombre del medicamento: ").lower()
+            dro = input("Ingrese la droga: ").lower()
+            os = input("Ingrese la Obra Social: ").lower()
+            plan = input("Ingrese el plan: ").lower()
+            imp = float(input("Ingrese el importe: "))
+            fyh = datetime.now()
+            cargarVenta(venta, cod, nom, dro, os, plan, imp, fyh)
+            agregarVenta(lista_ventas, venta)
+            print("Venta cargada con éxito. \n")
+            s = input("¿Desea cargar otra venta? si/no: ").lower()
+        print("\nFinalización de ingreso de nuevas ventas de medicamentos. \n")
 
     #Modificar una venta especifica de la Lista por Codigo del Medicamento
     elif ( opcion == 2):
@@ -48,8 +76,16 @@ while (opcion != 0):
     #Eliminar venta especifica de la Lista por Codigo de medicamento
     elif ( opcion == 3):
 
-    #Mostrar todas las ventas registradad ( imprimir lista de Ventas)
-    elif ( opcion == 4):
+    
+    elif (opcion == 4):
+    #Mostrar todas las ventas registradas
+        if (tamanio(lista_ventas) > 0):
+            print("Listado de todas las ventas registradas. \n")
+            for i in range(1, tamanio(lista_ventas)+1):
+                print("Datos de la venta n° " + i + "\n")
+                imprimirVenta(recuperarVenta(lista_ventas, i))
+        else:
+            print("No hay ventas registradas. \n")
 
     #Mostrar informe (total recaudado) de todas las Obras sociales
     elif ( opcion == 5):
@@ -63,8 +99,8 @@ while (opcion != 0):
     #mostrar las ventas registradas en el dia hasta una hora elegida
     elif ( opcion == 8):
 
-    opcion = int(input("Ingresar Opcion elegida: "))
+    opcion = MENU()
     
-print("El programa Finalizo")   
+print("El programa finalizó.")   
     
 
